a0ef12653d9195edcd8ba9a033ad7164bee350cc
Yoav Tzfati <yoav.tzfati@gmail.com>
2023-05-14 07:35:33 +0300

    works more consistently

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ use itertools::Itertools;
 use rand::{seq::SliceRandom, Rng};
 
 const IMAGE_SIZE: usize = 28 * 28;
-const LEARNING_RATE: f32 = 0.1;
 
 #[derive(Debug)]
 struct Node<const INPUT_SIZE: usize> {
@@ -40,25 +39,22 @@ impl<const INPUT_SIZE: usize> Node<INPUT_SIZE> {
         sum + self.bias
     }
     fn update(&mut self, input: &[f32; INPUT_SIZE], error: f32) {
-        self.bias -= LEARNING_RATE * error;
-        let input_sum: f32 = input.iter().filter(|&&v| v > 0.).sum();
+        self.bias -= error;
         self.weights
             .iter_mut()
             .zip_eq(input)
             .filter(|(_, &input)| input > 0.)
             .for_each(|(weight, &input)| {
-                *weight -= LEARNING_RATE * input * error / input_sum;
+                *weight -= input * error;
             });
     }
-    fn flush_updates(&mut self, updates: &mut Self, batch_size: u16) {
-        self.bias += updates.bias / f32::from(batch_size);
-        updates.bias = 0.;
+    fn flush_updates(&mut self, updates: &Self) {
+        self.bias += updates.bias;
         self.weights
             .iter_mut()
-            .zip_eq(&mut updates.weights)
+            .zip_eq(&updates.weights)
             .for_each(|(weight, update)| {
-                *weight += *update / f32::from(batch_size);
-                *update = 0.;
+                *weight += *update;
             });
     }
 }
@@ -96,12 +92,12 @@ impl<const INPUT_SIZE: usize, const OUTPUT_SIZE: usize> Layer<INPUT_SIZE, OUTPUT
                 node.update(input, *error);
             });
     }
-    fn flush_updates(&mut self, updates: &mut Self, batch_size: u16) {
+    fn flush_updates(&mut self, updates: &Self) {
         self.nodes
             .iter_mut()
-            .zip_eq(&mut updates.nodes)
+            .zip_eq(&updates.nodes)
             .for_each(|(node, update)| {
-                node.flush_updates(update, batch_size);
+                node.flush_updates(update);
             });
     }
 }
@@ -157,11 +153,23 @@ where
                     let activations = net.compute(input);
                     let errors = net.errors(&activations, label);
                     updates.update(input, &activations, &errors);
+                    if activations.output_layer.iter().copied().any(f32::is_nan)
+                        || !activations.output_layer_is_correct(label)
+                            && errors.hidden_layers[MIDDLE_LAYERS].iter().all(|&v| v == 0.)
+                    {
+                        dbg!(
+                            label,
+                            print_image(input),
+                            &activations,
+                            &errors,
+                            &net.output_layer.nodes[0],
+                            &net.middle_layers[0].nodes[0]
+                        );
+                        panic!()
+                    }
                 }
-                net.flush_updates(
-                    &mut updates,
-                    u16::try_from(chunk.len()).expect("chunk size overflows u16"),
-                );
+                net.flush_updates(&mut updates);
+                updates = Self::zeros();
                 bar.inc(1);
             }
         }
@@ -182,24 +190,72 @@ where
                 .update(&activations.hidden_layers[i], &errors.hidden_layers[i + 1]);
         }
         self.input_layer.update(
-            &input.map(|b| f32::from(b) / 255.),
+            &input.map(|b| f32::from(b) / 256.),
             &errors.hidden_layers[0],
         );
     }
-    fn flush_updates(&mut self, updates: &mut Self, batch_size: u16) {
-        self.input_layer
-            .flush_updates(&mut updates.input_layer, batch_size);
+    fn flush_updates(&mut self, updates: &mut Self) {
+        let sum_gradient = updates
+            .input_layer
+            .nodes
+            .iter()
+            .flat_map(|node| node.weights.iter().chain([&node.bias]))
+            .copied()
+            .map(f32::abs)
+            .sum::<f32>()
+            + updates
+                .middle_layers
+                .iter()
+                .map(|layer| {
+                    layer
+                        .nodes
+                        .iter()
+                        .flat_map(|node| node.weights.iter().chain([&node.bias]))
+                        .copied()
+                        .map(f32::abs)
+                        .sum::<f32>()
+                })
+                .sum::<f32>()
+            + updates
+                .output_layer
+                .nodes
+                .iter()
+                .flat_map(|node| node.weights.iter().chain([&node.bias]))
+                .copied()
+                .map(f32::abs)
+                .sum::<f32>();
+        let learning_coefficient = (10. / sum_gradient).min(1.);
+        updates.input_layer.nodes.iter_mut().for_each(|node| {
+            node.weights
+                .iter_mut()
+                .for_each(|w| *w *= learning_coefficient);
+            node.bias *= learning_coefficient;
+        });
+        updates.middle_layers.iter_mut().for_each(|layer| {
+            layer.nodes.iter_mut().for_each(|node| {
+                node.weights
+                    .iter_mut()
+                    .for_each(|w| *w *= learning_coefficient);
+                node.bias *= learning_coefficient;
+            });
+        });
+        updates.output_layer.nodes.iter_mut().for_each(|node| {
+            node.weights
+                .iter_mut()
+                .for_each(|w| *w *= learning_coefficient);
+            node.bias *= learning_coefficient;
+        });
+        self.input_layer.flush_updates(&updates.input_layer);
         for i in 0..MIDDLE_LAYERS {
-            self.middle_layers[i].flush_updates(&mut updates.middle_layers[i], batch_size);
+            self.middle_layers[i].flush_updates(&updates.middle_layers[i]);
         }
-        self.output_layer
-            .flush_updates(&mut updates.output_layer, batch_size);
+        self.output_layer.flush_updates(&updates.output_layer);
     }
     fn compute(&self, input: &[u8; IMAGE_SIZE]) -> Activations<LAYER_SIZE, { MIDDLE_LAYERS + 1 }> {
         let mut hidden_layers = [[0.; LAYER_SIZE]; MIDDLE_LAYERS + 1];
         hidden_layers[0] = self
             .input_layer
-            .compute(&input.map(|b| f32::from(b) / 255.));
+            .compute(&input.map(|b| f32::from(b) / 256.));
         for i in 0..MIDDLE_LAYERS {
             hidden_layers[i + 1] = self.middle_layers[i].compute(&hidden_layers[i]);
         }
@@ -220,10 +276,11 @@ where
             .zip_eq(&activations.output_layer)
             .enumerate()
             .for_each(|(i, (error, &act))| {
+                let layer_size = f32::from(u16::try_from(LAYER_SIZE).unwrap());
                 *error = if i == label as usize {
-                    act.min(16.) - 16.
+                    10. * (act.min(layer_size) - layer_size)
                 } else {
-                    act.max(-16.) + 16.
+                    act.max(0.)
                 };
             });
         let mut hidden_layers = [[0.; LAYER_SIZE]; MIDDLE_LAYERS + 1];
@@ -303,7 +360,7 @@ fn main() {
         .copied()
         .zip_eq(train_images.as_chunks().0)
         .collect();
-    let nn = NeuralNet::<16, 1>::train(data, 4);
+    let nn = NeuralNet::<64, 1>::train(data, 20);
 
     let errors = test_labels
         .iter()
